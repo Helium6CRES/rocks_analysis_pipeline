ff63a74f7ed2efb83f90ee6197018c0ec63d06ee
drewbyron <william.andrew.byron@gmail.com>
2022-09-08 08:39:31 -0700

    Fixing the analysis_index to be consistent across all run_ids in a given analysis run. This will help with cleanup

--- katydid_on_rocks_dev_qsub.py
+++ katydid_on_rocks_dev_qsub.py
@@ -3,7 +3,7 @@ import time
 import argparse
 import pandas as pd
 import datetime
-from glob import glob 
+from glob import glob
 import subprocess as sp
 from shutil import copyfile
 import psycopg2
@@ -15,59 +15,94 @@ from pathlib import Path
 import yaml
 import he6_cres_spec_sims.spec_tools.spec_calc.spec_calc as sc
 
-pd.set_option('display.max_columns', 100)
+pd.set_option("display.max_columns", 100)
+
 
 def main():
     """
+
+        TODOS:
+    * Need to source .bashrc for root to work properly. WORKS
+    * Need to have the permissions such that the folders are writeable by others in
+    our group.
+    * Currently we can make directories and make config files and verrrrrrry useful data frame columns.
+    * Deal with group permissions. We need to be sure that root files that are built can be accessed/edited by all users in our group.
+    * Get katydid working in a place we can all use it.
+    * Write first copy of config file to root analysis dir.
+    * WINSTON NEEDS TO FIGURE OUT PERMISSIONS!!!! Katydid isnt working for other users. JK
+    * make it work and get it running.
+    * Build out a way for people to interact with all these root files in a nice way.
+    This is where the python post processing comes in and we pickle the file in rocks.
+    Then anyone can open that pickle file and be on there way to working on an analysis.
+    * sp.run("chmod shit that deals with permissions WINSTON!!!!")
+    * Write first copy of config file to root analysis dir.
+
     """
 
-    # need to source .bashrc for root to work properly
-    # source = sp.run(['source /data/eliza4/he6_cres/.bashrc'], executable='/bin/bash')
-    # Handling permissions issues when creating files/directories on rocks
-    # sg doesn't work, takes user out of the script, umask has this behavior but shell=True got rid of it
-    # sg = sp.run(['sg he6_cres'], executable='/bin/bash', shell=True)
-    umask = sp.run(['umask u=rwx,g=rwx,o=rx'], executable='/bin/bash', shell=True)
+    umask = sp.run(["umask u=rwx,g=rwx,o=rx"], executable="/bin/bash", shell=True)
 
+    # Parse command line arguments.
     par = argparse.ArgumentParser()
     arg = par.add_argument
-    arg('-id', '--run_id', type=int, help='run_id to run katydid on.')
-    arg('-b','--base_config', type=str, help='base .yaml katydid config file to be run on run_id, should exist in base config directory.')
-    arg('-fn', '--file_num', default=-1, type=int, help='Number of files in run id to analyze (<= number of files in run_id)')
-    arg('-c', '--clean_up', default = False, type=bool, help = 'If true a clean_up run will be run.  ')
+    arg("-id", "--run_id", type=int, help="run_id to run katydid on.")
+    arg(
+        "-ai",
+        "--analysis_index",
+        type=int,
+        help="analysis_index used to label directories.",
+    )
+    arg(
+        "-b",
+        "--base_config",
+        type=str,
+        help="base .yaml katydid config file to be run on run_id, should exist in base config directory.",
+    )
+    arg(
+        "-fn",
+        "--file_num",
+        default=-1,
+        type=int,
+        help="Number of files in run id to analyze (<= number of files in run_id)",
+    )
+    arg(
+        "-c",
+        "--clean_up",
+        default=False,
+        type=bool,
+        help="If true a clean_up run will be run.",
+    )
     args = par.parse_args()
 
     print(f"run_id: {args.run_id}")
     print(f"base_config: {args.base_config}")
-   
+
     file_df = create_file_df(args.run_id)
     file_df["file_num"] = file_df.index
-    file_df["rocks_file_path"] = file_df['file_path'].apply(lambda x: process_fp(x))
+    file_df["rocks_file_path"] = file_df["file_path"].apply(lambda x: process_fp(x))
     file_df["exists"] = file_df["rocks_file_path"].apply(lambda x: check_if_exists(x))
-    file_df["approx_slope"] = get_slope(file_df["true_field"][0] )
+    file_df["approx_slope"] = get_slope(file_df["true_field"][0])
 
-    dbscan_r = get_dbscan_radius(file_df["approx_slope"][0] )
+    dbscan_r = get_dbscan_radius(file_df["approx_slope"][0])
     file_df["dbscan_radius_0"] = dbscan_r[0]
     file_df["dbscan_radius_1"] = dbscan_r[1]
 
     file_df["base_config_path"] = get_base_config_path(args.base_config)
-    file_df["output_dir"] = build_dir_structure(args.run_id)
+    file_df["output_dir"] = build_dir_structure(args.run_id, args.analysis_index)
 
     # TODO: Change this to work.
     file_df["rocks_noise_file_path"] = file_df["rocks_file_path"]
 
-    file_df["root_file_path"] = file_df.apply(lambda row: build_root_file_path(row), axis = 1)
+    file_df["root_file_path"] = file_df.apply(
+        lambda row: build_root_file_path(row), axis=1
+    )
 
-    # print(file_df["root_file_path"][0])
     if args.file_num == -1:
-        file_df.apply(lambda row: run_katydid(row), axis = 1)
+        file_df.apply(lambda row: run_katydid(row), axis=1)
     else:
-        file_df[:args.file_num].apply(lambda row: run_katydid(row), axis = 1)
-
-    #TODO: Write first copy of config file to root analysis dir. 
+        file_df[: args.file_num].apply(lambda row: run_katydid(row), axis=1)
 
 
 def run_katydid(file_df):
-
 
     base_config_path = Path(file_df["base_config_path"])
 
@@ -78,12 +113,15 @@ def run_katydid(file_df):
     # Copy the katydid config file (in same dir) so that we can write to the copy not
     # the original.
     config_path = base_config_path.parent / str(
-        base_config_path.stem + "_copy_" + str(file_df["run_id"]) + base_config_path.suffix
+        base_config_path.stem
+        + "_copy_"
+        + str(file_df["run_id"])
+        + base_config_path.suffix
     )
     copyfile(base_config_path, config_path)
     print(config_path)
 
-    # TODO: input noise file path. 
+    # TODO: input noise file path.
     config_dict["spec1"]["filename"] = file_df["rocks_noise_file_path"]
     config_dict["spec2"]["filename"] = file_df["rocks_file_path"]
 
@@ -102,7 +140,6 @@ def run_katydid(file_df):
                     file_df["dbscan_radius_1"],
                 ]
 
-
     # Dump the altered config_dict into the copy of the config file.
     # Note that the comments are all lost because you only write the contents of the
     # confic dict.
@@ -113,32 +150,24 @@ def run_katydid(file_df):
     # Note that you need to have Katydid configured as a bash executable for this to
     # work (as is standard).
     run_katydid = sp.run(
-        ["/data/eliza4/he6_cres/katydid/build/bin/Katydid","-c" , config_path],
-        capture_output=True
+        ["/data/eliza4/he6_cres/katydid/build/bin/Katydid", "-c", config_path],
+        capture_output=True,
     )
     # print("\nspec file {} of {}".format(i + 1, len(spec_files)))
     print("Katydid output:", run_katydid.stdout[-100:])
 
-    # Print statement to 
+    # Print statement to
     now = datetime.datetime.now()
-    print("file {file_df.index}. time: {}. root file {}".format(file_df["file_num"], now, file_df["rocks_file_path"]))
+    print(
+        "file {file_df.index}. time: {}. root file {}".format(
+            file_df["file_num"], now, file_df["rocks_file_path"]
+        )
+    )
 
     # Delete the copy of the katydid config file once done with processing.
-    Path(config_path).unlink()    
+    Path(config_path).unlink()
 
     return None
-
-    # TODOS: 
-    # * Currently we can make directories and make config files and verrrrrrry useful data frame columns. 
-    # * Deal with group permissions. We need to be sure that root files that are built can be accessed/edited by all users in our group.
-    # * Get katydid working in a place we can all use it. 
-    # * Write first copy of config file to root analysis dir.
-    # * WINSTON NEEDS TO FIGURE OUT PERMISSIONS!!!! Katydid isnt working for other users. JK
-    # * make it work and get it running.
-    # * Build out a way for people to interact with all these root files in a nice way. 
-    # This is where the python post processing comes in and we pickle the file in rocks. 
-    # Then anyone can open that pickle file and be on there way to working on an analysis.
-    # * sp.run("chmod shit that deals with permissions WINSTON!!!!")
 
 
 def build_root_file_path(file_df):
@@ -148,9 +177,10 @@ def build_root_file_path(file_df):
 
     return str(root_path)
 
+
 def get_base_config_path(base_config):
 
-    base_path = Path('/data/eliza4/he6_cres/katydid_analysis/base_configs')
+    base_path = Path("/data/eliza4/he6_cres/katydid_analysis/base_configs")
     base_config = base_path / Path(base_config)
 
     if not base_config.is_file():
@@ -159,7 +189,7 @@ def get_base_config_path(base_config):
     return str(base_config)
 
 
-def get_slope(true_field, frequency: float = 18.5e9 ): 
+def get_slope(true_field, frequency: float = 18.5e9):
 
     approx_power = sc.power_larmor(true_field, frequency)
     approx_energy = sc.freq_to_energy(frequency, true_field)
@@ -167,7 +197,10 @@ def get_slope(true_field, frequency: float = 18.5e9 ):
 
     return approx_slope
 
-def get_dbscan_radius(approx_slope: float, dbscan_base_radius: List[float]  = [5.0e-4, 40e6]) -> List[float]: 
+
+def get_dbscan_radius(
+    approx_slope: float, dbscan_base_radius: List[float] = [5.0e-4, 40e6]
+) -> List[float]:
     """
     Parameters
     ----------
@@ -175,62 +208,66 @@ def get_dbscan_radius(approx_slope: float, dbscan_base_radius: List[float]  = [5
         The slope of the tracks.
     dbscan_base_radii : List[float]
         Two elememnt list corresponding to the ideal dbscan radius for 8e10 Hz/s.
-        Ideally this wouldn't be a hardcoded requirement but not sure how else to do it. 
+        Ideally this wouldn't be a hardcoded requirement but not sure how else to do it.
 
     Returns
     ------
     dbscan_radii : List[float]
         Two elememnt list corresponding to the ideal dbscan radii for given slope.
-        
+
     """
-    
-    dbscan_radius = [dbscan_base_radius[1] / approx_slope , dbscan_base_radius[0] *approx_slope]
+
+    dbscan_radius = [
+        dbscan_base_radius[1] / approx_slope,
+        dbscan_base_radius[0] * approx_slope,
+    ]
 
     return dbscan_radius
 
 
+def run_id_to_slope(run_id: int, frequency: float = 18.0e9):
 
-def run_id_to_slope(run_id: int, frequency: float = 18.0e9): 
-    
-    
-    query = '''SELECT * FROM he6cres_runs.run_log
+    query = """SELECT * FROM he6cres_runs.run_log
            WHERE run_log.run_id = {}
            ORDER BY run_id DESC LIMIT 10
-            '''.format(run_id)
-    
+            """.format(
+        run_id
+    )
+
     run_id_info = db.he6cres_db_query(query)
-    
+
     if not (len(run_id_info) == 1):
-        raise ValueError(f'There seems to be no run with run_id = {run_id}.')
-        
-    true_field = run_id_info.true_field[0] 
+        raise ValueError(f"There seems to be no run with run_id = {run_id}.")
+
+    true_field = run_id_info.true_field[0]
     approx_power = sc.power_larmor(true_field, frequency)
     approx_energy = sc.freq_to_energy(frequency, true_field)
     approx_slope = sc.df_dt(approx_energy, true_field, approx_power)
-    
+
     return approx_slope
 
 
-def process_fp(daq_fp): 
-    rocks_fp = '/data/eliza4/he6_cres/' + daq_fp[5:]
+def process_fp(daq_fp):
+    rocks_fp = "/data/eliza4/he6_cres/" + daq_fp[5:]
     return rocks_fp
 
-def check_if_exists(rocks_fp): 
+
+def check_if_exists(rocks_fp):
     rocks_fp = Path(rocks_fp)
     return rocks_fp.is_file()
 
-def build_dir_structure(run_id): 
 
-    base_path = Path('/data/eliza4/he6_cres/katydid_analysis/root_files')
+def build_dir_structure(run_id, analysis_index):
 
-    run_id_dir = base_path / Path(f"run_id_{run_id}") 
+    base_path = Path("/data/eliza4/he6_cres/katydid_analysis/root_files")
+
+    run_id_dir = base_path / Path(f"run_id_{run_id}")
 
     if not run_id_dir.is_dir():
-        run_id_dir.mkdir()
-        print(f"Created directory: {run_id_dir}")
+    	raise UserWarning("This directory should have been made already.")
+        # run_id_dir.mkdir()
+        # print(f"Created directory: {run_id_dir}")
 
-    analysis_dirs = glob(str(run_id_dir) + "/*/")
-    analysis_index = len(analysis_dirs)
     current_analysis_dir = run_id_dir / Path(f"analysis_{analysis_index:03d}")
     if not current_analysis_dir.is_dir():
         current_analysis_dir.mkdir()
@@ -243,31 +280,31 @@ def create_file_df(run_id: int):
     """
     here we could set up a dataframe from the He6-CRES database
     """
-    query_he6_db = '''
+    query_he6_db = """
                     SELECT r.run_id, f.spec_id, f.file_path, r.true_field
                     FROM he6cres_runs.run_log as r
                     RIGHT JOIN he6cres_runs.spec_files as f
                     ON r.run_id = f.run_id
                     WHERE r.run_id = {}
                     ORDER BY r.created_at DESC
-                  '''.format(run_id)
-                  
+                  """.format(
+        run_id
+    )
+
     file_df = he6cres_db_query(query_he6_db)
 
-
-    return  file_df
+    return file_df
 
 
 def run_chunk():
-    """
-    """
+    """ """
     pass
 
     # run_id = 1 # pass this in
 
     # # load your input dataframe
     # f_list = glob('/data/eliza4/he6_cres/**', recursive=True)
-    
+
     # df = pd.DataFrame(f_list, columns=['fname', 'config_file'])
 
     # # select the chunk to process
@@ -290,23 +327,23 @@ def run_chunk():
     # df_chunk.apply(run_katydid, axis=1)
 
 
-
-
-# Simplify to not have an insert capability. 
-def he6cres_db_query(query: str ) -> typing.Union[None, pd.DataFrame]: 
+# Simplify to not have an insert capability.
+def he6cres_db_query(query: str) -> typing.Union[None, pd.DataFrame]:
 
     connection = False
     try:
         # Connect to an existing database
-        connection = psycopg2.connect(user="postgres",
-                                      password="chirality",
-                                      host="wombat.npl.washington.edu",
-                                      port="5544",
-                                      database="he6cres_db")
+        connection = psycopg2.connect(
+            user="postgres",
+            password="chirality",
+            host="wombat.npl.washington.edu",
+            port="5544",
+            database="he6cres_db",
+        )
 
         # Create a cursor to perform database operations
         cursor = connection.cursor()
-        print("Successfully connected to he6cres_db") 
+        print("Successfully connected to he6cres_db")
 
         # Execute a sql_command
         cursor.execute(query)
@@ -314,11 +351,11 @@ def he6cres_db_query(query: str ) -> typing.Union[None, pd.DataFrame]:
         query_result = pd.DataFrame(cursor.fetchall(), columns=cols)
 
         print("Query executed.")
-        
+
     except (Exception, Error) as error:
         print("Error while connecting to he6cres_db", error)
         query_result = None
-        
+
     finally:
         if connection:
             cursor.close()
@@ -327,5 +364,6 @@ def he6cres_db_query(query: str ) -> typing.Union[None, pd.DataFrame]:
 
     return query_result
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- qsub_katydid.py
+++ qsub_katydid.py
@@ -3,6 +3,7 @@ import subprocess as sp
 import argparse
 from typing import List
 
+
 def main():
     """
     helper script to run arbitrary jobs from command line on the rocks cluster.
@@ -18,27 +19,43 @@ def main():
     $ ./qsub.py -c --job 'python3 /home/wisecg/he6_files.py -c 01'
     """
     par = argparse.ArgumentParser()
-    arg, st = par.add_argument, 'store_true'
+    arg, st = par.add_argument, "store_true"
     # arg('--job', nargs=1, type=str, help='command to execute, usually in quotes')
-    arg("-t", "--tlim", nargs=1, type=str, help='set time limit (HH:MM:SS)')
-    arg('-rids', '--runids', nargs='+', type=int, help='run ids to analyze')
-    arg('-b','--base_config', type=str, help='base .yaml katydid config file to be run on run_id, should exist in base config directory.')
-    arg('-fn', '--file_num', default=-1, type=int, help='Number of files in run id to analyze (<= number of files in run_id)')
+    arg("-t", "--tlim", nargs=1, type=str, help="set time limit (HH:MM:SS)")
+    arg("-rids", "--runids", nargs="+", type=int, help="run ids to analyze")
+    arg(
+        "-b",
+        "--base_config",
+        type=str,
+        help="base .yaml katydid config file to be run on run_id, should exist in base config directory.",
+    )
+    arg(
+        "-fn",
+        "--file_num",
+        default=-1,
+        type=int,
+        help="Number of files in run id to analyze (<= number of files in run_id)",
+    )
 
     args = par.parse_args()
 
-    tlim = '12:00:00' if args.tlim is None else args.tlim[0]
+    tlim = "12:00:00" if args.tlim is None else args.tlim[0]
 
-    # cmd = args.job[0]
+    # Get the analysis index to use for the list of jobs. 
+    analysis_index = get_analysis_index(args.runids)
+    print(f"analysis_index: {analysis_index}")
 
-    #Note: the \n must be a literal thing not a \n in the python string itself. Be careful with this.
-    con = "\"singularity exec --bind /data/eliza4/he6_cres/ /data/eliza4/he6_cres/containers/he6cres-katydid-base.sif /bin/bash -c $\'source /data/eliza4/he6_cres/.bashrc {} ".format(r"\n")
-    # cmd = con + f"{args.job[0]}\'\""
+    # Note: the \n must be a literal thing not a \n in the python string itself. Be careful with this.
+    con = "\"singularity exec --bind /data/eliza4/he6_cres/ /data/eliza4/he6_cres/containers/he6cres-katydid-base.sif /bin/bash -c $'source /data/eliza4/he6_cres/.bashrc {} ".format(
+        r"\n"
+    )
 
-#    if args.job:
+
     for run_id in args.runids:
-        default_katydid_sub = 'python3 /data/eliza4/he6_cres/python_dev/katydid_on_rocks_dev_qsub.py -id {} -b \"{}\" -fn {}'.format(run_id, args.base_config, args.file_num)
-        cmd = con + f"{default_katydid_sub}\'\""
+        default_katydid_sub = 'python3 /data/eliza4/he6_cres/python_dev/katydid_on_rocks_dev_qsub.py -id {} -ai {} -b "{}" -fn {} '.format(
+            run_id, analysis_index, args.base_config, args.file_num
+        )
+        cmd = con + f"{default_katydid_sub}'\""
         qsub_job(run_id, cmd, tlim)
 
 
@@ -50,35 +67,53 @@ def qsub_job(run_id, cmd, tlim):
     https://stackoverflow.com/questions/15089315/redirect-output-to-different-files-for-sun-grid-engine-array-jobs-sge
     """
     qsub_opts = [
-        "-S /bin/bash", # use bash
-        "-cwd", # run from current working directory
-        "-m n", # don't send email notifications
-        "-w e", # verify syntax
-        "-V", # inherit environment variables
-        "-N cres_analysis", # job name
-        f"-l h_rt={tlim}", # time limit
-        "-q all.q", # queue name (cenpa only uses one queue)
-        "-j yes", # join stderr and stdout
-        "-b y", # Look for series of bytes.
+        "-S /bin/bash",  # use bash
+        "-cwd",  # run from current working directory
+        "-m n",  # don't send email notifications
+        "-w e",  # verify syntax
+        "-V",  # inherit environment variables
+        f"-N run_id_{run_id}",  # job name
+        f"-l h_rt={tlim}",  # time limit
+        "-q all.q",  # queue name (cenpa only uses one queue)
+        "-j yes",  # join stderr and stdout
+        "-b y",  # Look for series of bytes.
         "-o /data/eliza4/he6_cres/job_logs/output_{}.txt".format(run_id),
         # "-t {}-{}".format(1,len(run_ids)) # job array mode.  example: 128 jobs w/ label $SGE_TASK_ID
     ]
-    qsub_str = ' '.join([str(s) for s in qsub_opts])
+    qsub_str = " ".join([str(s) for s in qsub_opts])
     batch_cmd = "qsub {} {}".format(qsub_str, cmd)
 
-    # Or maybe we should just do a for loop to submit the jobs.
-
-    # add singularity command
-
-    # Notes from Sam:
-
-    # $SGE_TASK_ID is an env variable that we can use to
+    print("\n\n", batch_cmd, "\n\n")
+    sp.run(batch_cmd, shell=True)
 
 
-    print("\n\n", batch_cmd,"\n\n")
-    sp.run(batch_cmd, shell = True)
+def get_analysis_index(run_ids):
+
+    """
+    We want each analysis run simultaneously to have the same analysis number.
+    This function goes through and builds the directory structure out and
+    checks to see what the next possible analysis index is such that all run_ids
+    recieve the same analysis index.
+
+    """
+
+    base_path = Path("/data/eliza4/he6_cres/katydid_analysis/root_files")
+
+    analysis_indices = []
+    for run_id in args.runids:
+
+        run_id_dir = base_path / Path(f"run_id_{run_id}")
+
+        if not run_id_dir.is_dir():
+            run_id_dir.mkdir()
+            print(f"Created directory: {run_id_dir}")
+
+        analysis_dirs = glob(str(run_id_dir) + "/*/")
+        analysis_index = len(analysis_dirs)
+        analysis_indices.append(analysis_index)
+
+    return max(analysis_indices)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
