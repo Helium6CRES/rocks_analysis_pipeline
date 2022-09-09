1dd753e9592a7450b797534eaafce7d8e878f2fe
drewbyron <william.andrew.byron@gmail.com>
2022-09-09 08:12:13 -0700

    clean-up script

--- run_katydid.py
+++ run_katydid.py
@@ -76,8 +76,8 @@ def main():
     print(f"run_id: {args.run_id}")
     print(f"base_config: {args.base_config}")
 
-    # Deal with permissions (chmod 770)
-    set_permissions()
+    # # Deal with permissions (chmod 770)
+    # set_permissions()
 
     # If the rid_df exists then it is a clean-up run:
     file_df_path = build_file_df_path(args.run_id, args.analysis_id)
@@ -234,10 +234,12 @@ def clean_up_root_dir(file_df):
 
 
 def set_permissions():
-    permissions = sp.run(
-        ["chmod -R 774 katydid_analysis/"],
-        capture_output=False,
-    )
+
+    set_group = sp.run(["chgrp","-R","he6_cres","katydid_analysis/"])
+    set_permission = sp.run(["chmod", "-R", "774", "katydid_analysis/"])
+
+    return None 
+
 
 
 def root_file_check(file_df):
