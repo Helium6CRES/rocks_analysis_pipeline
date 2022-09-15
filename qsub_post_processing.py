b235b9a21a20773fce8107316ea66df35d5c34ba
drewbyron <william.andrew.byron@gmail.com>
2022-09-15 10:41:21 -0700

    Getting post processing working as an sge job

--- qsub_post_processing.py
+++ qsub_post_processing.py
@@ -180,7 +180,7 @@ def qsub_job(experiment_name, analysis_id, cmd, tlim):
         "-q all.q",  # queue name (cenpa only uses one queue)
         "-j yes",  # join stderr and stdout
         "-b y",  # Look for series of bytes.
-        f"-o /data/eliza4/he6_cres/katydid_analysis/job_logs/run_post_processing/{experiment_name}_aid_{analysis_id}.txt",
+        f"-o /data/eliza4/he6_cres/katydid_analysis/job_logs/post_processing/{experiment_name}_aid_{analysis_id}.txt",
         # "-t {}-{}".format(1,len(run_ids)) # job array mode.  example: 128 jobs w/ label $SGE_TASK_ID
     ]
     qsub_str = " ".join([str(s) for s in qsub_opts])
